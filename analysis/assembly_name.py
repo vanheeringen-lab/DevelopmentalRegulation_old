370d97cf8865a29bcf70e5a00a80db5e761e6a03
Maarten van der Sande <maartenvandersande@hotmail.com>
2020-09-23 16:31:27 +0200

    lets try github action

---
+++ analysis/assembly_name.py
@@ -0,0 +1,24 @@
+ass2name = {
+    "Astyanax_mexicanus-2.0": "cavefish",
+    "Bter_1.0": "Bumblebee",
+    "ARS-UCD1.2": "cow",
+    "BraLan2": "amphioxus",
+    "Bl71nemr": "amphioxus",
+    "ce11": "nematode",
+    "KH": "sea vase",
+    "danRer11": "zebrafish",
+    "dm6": "fruit fly",
+    "GRCg6a": "chicken",
+    "hydra": "hydra",
+    "GRCm39": "mouse",
+    "mm10": "mouse",
+    "ASM20922v1": "sea anemone",
+    "ASM223467v1": "medaka",
+    "Phmamm_MTP2014": "sea squirt",
+    "HLtupMer6": "tegu",
+    "ASM318616v1": "turbot",
+    "Spur_5.0": "sea urchin",
+    "xenTro9": "frog",
+}
+
+name2ass = {v: k for k, v in ass2name.items()}
